f40dc69cfa4fbce14252b82f27983292d8c7b4fc
anantha.kommaraju <anantha.kommaraju@lloydsbanking.dev>
2025-05-07 06:45:24 +0000

    Cleaned up

--- generative-appraisal/src/main/java/com/consumerlending/generative/appraisal/repository/AppraisalSummaryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.consumerlending.generative.appraisal.repository;
-
-import com.consumerlending.generative.appraisal.domain.AppraisalSummary;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppraisalSummaryRepository extends JpaRepository<AppraisalSummary, Long> {
-}
